a244c8be270c458f60fe373d020fad0fe9f14e41
Филипп Баннов <bannov.2002@bk.ru>
2022-04-19 10:30:30 +0300

    Добавьте файлы проекта.

---
+++ RestApi/RestApi/IRest/IRestService.cs
@@ -0,0 +1,13 @@
+﻿using RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApi.IRest
+{
+	public interface IRestService
+	{
+		Task<List<TodoItem>> RefreshDataAsync();
+	}
+}
---
+++ RestApi/RestApi/IRest/TodoItemManager.cs
@@ -0,0 +1,23 @@
+﻿using RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApi.IRest
+{
+	public class TodoItemManager
+	{
+		IRestService restService;
+
+		public TodoItemManager(IRestService service)
+		{
+			restService = service;
+		}
+
+		public Task<List<TodoItem>> GetTasksAsync()
+		{
+			return restService.RefreshDataAsync();
+		}
+	}
+}
